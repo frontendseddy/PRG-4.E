28256ca879242ea714f9197b60a14bd953f5967b
frontendseddy <pan.bramboru@seznam.cz>
2024-01-23 21:02:49 +0100

    error

--- src/SimonSays/SimonWindow.java
+++ src/SimonSays/SimonWindow.java
@@ -7,44 +7,14 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class SimonWindow extends JFrame {
-    JButton startButton, replayButton ;
+    final int DIFICULTY = 5;
+    JButton startButton, replayButton;
     JPanel gridPanel, buttonsPanel;
     String sequence;
     String input = "";
     ArrayList<SimonButton> buttons = new ArrayList<>();
-    final int DIFICULTY = 5;
 
-    String getSequence(int length){
-        String sequence = "";
-        for (int i = 0; i < length; i++) {
-            sequence += buttons.get((int) (Math.random()*buttons.size())).getText();
-        }
-        System.out.println("Cheat:" + sequence);
-        return sequence;
-    }
-
-    void highlightSequence(int time, Color color)  {
-        Color original;
-        for (int i = 0; i < sequence.length(); i++) {
-            for(JButton toHighlight : buttons){
-                if (toHighlight.getText().charAt(0) == sequence.charAt(i)){
-                    original = toHighlight.getBackground();
-                    toHighlight.setBackground(color);
-                    toHighlight.paintImmediately(0,0,getWidth(),getHeight());
-                    try {
-                        Thread.sleep(time);
-                    } catch (InterruptedException e) {
-                        System.out.println("Thread died :/");
-                    }
-                    toHighlight.setBackground(original);
-                    toHighlight.paintImmediately(0,0,getWidth(),getHeight());
-
-                }
-            }
-        }
-    }
-
-    SimonWindow(){
+    SimonWindow() {
         this.setSize(700, 700);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
@@ -54,22 +24,22 @@ public class SimonWindow extends JFrame {
         startButton = new JButton("Play");
         gridPanel = new JPanel();
         buttonsPanel = new JPanel();
-        buttonsPanel.setLayout(new GridLayout(1,2));
+        buttonsPanel.setLayout(new GridLayout(1, 2));
         replayButton.setEnabled(false);
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 replayButton.setEnabled(true);
                 sequence = getSequence(DIFICULTY);
-                highlightSequence(500,new Color(0x03F825));
+                highlightSequence(500, new Color(0x03F825));
 //                gridPanel.paintImmediately(0,0,getWidth(),getHeight());
                 input = "";
             }
         });
 
-        gridPanel.setLayout(new GridLayout(3,3));
+        gridPanel.setLayout(new GridLayout(3, 3));
         for (int i = 0; i < 9; i++) {
-            SimonButton toAdd = new SimonButton(String.valueOf(i+1));
+            SimonButton toAdd = new SimonButton(String.valueOf(i + 1));
             gridPanel.add(toAdd);
             buttons.add(toAdd);
             toAdd.addActionListener(new ActionListener() {
@@ -78,7 +48,7 @@ public class SimonWindow extends JFrame {
                     input += toAdd.getText();
                     if (input.length() == DIFICULTY) {
                         boolean win;
-                        if (sequence.equals(input)){
+                        if (sequence.equals(input)) {
                             win = true;
                         } else {
                             win = false;
@@ -99,38 +69,70 @@ public class SimonWindow extends JFrame {
         replayButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                highlightSequence(1000,Color.orange);
+                highlightSequence(1000, Color.orange);
             }
         });
         this.add(gridPanel, BorderLayout.CENTER);
-    }
-
-    void evaluate(boolean win) {
-        int option = JOptionPane.showConfirmDialog(null, "tak si uplnej kokot", "test", JOptionPane.);
-        System.out.println(option);
-        if (option == 0){
-
-        }
     }
 
     public static void main(String[] args) {
         new SimonWindow().setVisible(true);
     }
 
+    String getSequence(int length) {
+        String sequence = "";
+        for (int i = 0; i < length; i++) {
+            sequence += buttons.get((int) (Math.random() * buttons.size())).getText();
+        }
+        System.out.println("Cheat:" + sequence);
+        return sequence;
+    }
+
+    void highlightSequence(int time, Color color) {
+        Color original;
+        for (int i = 0; i < sequence.length(); i++) {
+            for (JButton toHighlight : buttons) {
+                if (toHighlight.getText().charAt(0) == sequence.charAt(i)) {
+                    original = toHighlight.getBackground();
+                    toHighlight.setBackground(color);
+                    toHighlight.paintImmediately(0, 0, getWidth(), getHeight());
+                    try {
+                        Thread.sleep(time);
+                    } catch (InterruptedException e) {
+                        System.out.println("Thread died :/");
+                    }
+                    toHighlight.setBackground(original);
+                    toHighlight.paintImmediately(0, 0, getWidth(), getHeight());
+
+                }
+            }
+        }
+    }
+
+    void evaluate(boolean win) {
+        int option = JOptionPane.showConfirmDialog(null, "tak si uplnej kokot", "test", JOptionPane.YES_NO_OPTION);
+        System.out.println(option);
+        if (option == 0) {
+
+        }
+    }
+
     void resetGame() {
         replayButton.setEnabled(true);
         sequence = getSequence(DIFICULTY);
-        highlightSequence(500,new Color(0x03F825));
+        highlightSequence(500, new Color(0x03F825));
     }
 
-} class SimonButton extends JButton{
+}
 
-    SimonButton(String text){
+class SimonButton extends JButton {
+
+    SimonButton(String text) {
         this.setText(text);
         this.setBackground(Color.cyan);
         this.setForeground(Color.black);
         this.setOpaque(true);
-        this.setBorder(BorderFactory.createLineBorder(Color.black,2));
+        this.setBorder(BorderFactory.createLineBorder(Color.black, 2));
         this.setFont(new Font("Consolas", Font.BOLD, 32));
         this.setFocusable(false);
     }
