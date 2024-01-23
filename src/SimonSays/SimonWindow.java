e4edc6e0f44fb29b6d41cc4ce9b374840b5d988e
frontendseddy <pan.bramboru@seznam.cz>
2024-01-23 21:00:10 +0100

    something else

--- src/SimonSays/SimonWindow.java
+++ src/SimonSays/SimonWindow.java
@@ -7,95 +7,130 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class SimonWindow extends JFrame {
-    JButton startButton;
+    JButton startButton, replayButton ;
     JPanel gridPanel, buttonsPanel;
-    ArrayList<SimonButton> somet = new ArrayList<SimonButton>();
     String sequence;
+    String input = "";
+    ArrayList<SimonButton> buttons = new ArrayList<>();
+    final int DIFICULTY = 5;
 
-    public SimonWindow() {
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(700, 700);
-        this.setLayout(new BorderLayout());
-        this.setLocationRelativeTo(null);
-
-        startButton = new JButton("PLAY");
-
-        buttonsPanel = new JPanel();
-        buttonsPanel.setLayout(new GridLayout(1, 2));
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sequence = getSequence(5);
-                highlightSequence(300, new Color(0x6EEC07));
-//                gridPanel.paintImmediately(0,0, getWidth(), getHeight());
-            }
-        });
-        JButton resetBTN = new JButton("RESET");
-
-        gridPanel = new JPanel();
-        gridPanel.setLayout(new GridLayout(3, 3));
-
-        for (int i = 0; i < 9; i++) {
-            SimonButton toAdd = new SimonButton(String.valueOf(i + 1));
-            gridPanel.add(toAdd);
-            somet.add(toAdd);
-            toAdd.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-
-                }
-            });
-        }
-
-        this.add(buttonsPanel, BorderLayout.NORTH);
-        buttonsPanel.add(startButton);
-        this.add(gridPanel, BorderLayout.CENTER);
-
-    }
-
-    public static void main(String[] args) {
-        new SimonWindow().setVisible(true);
-    }
-
-    String getSequence(int length) {
+    String getSequence(int length){
         String sequence = "";
         for (int i = 0; i < length; i++) {
-            sequence += somet.get((int) (Math.random() * somet.size())).getText();
+            sequence += buttons.get((int) (Math.random()*buttons.size())).getText();
         }
-        System.out.println("Cheat: " + sequence);
+        System.out.println("Cheat:" + sequence);
         return sequence;
     }
 
-    void highlightSequence(int time, Color color) {
+    void highlightSequence(int time, Color color)  {
         Color original;
         for (int i = 0; i < sequence.length(); i++) {
-            for (JButton toHighlight : somet) {
-                if (toHighlight.getText().charAt(0) == sequence.charAt(i)) {
+            for(JButton toHighlight : buttons){
+                if (toHighlight.getText().charAt(0) == sequence.charAt(i)){
                     original = toHighlight.getBackground();
                     toHighlight.setBackground(color);
-                    toHighlight.paintImmediately(0, 0, getWidth(), getHeight());
+                    toHighlight.paintImmediately(0,0,getWidth(),getHeight());
                     try {
                         Thread.sleep(time);
                     } catch (InterruptedException e) {
                         System.out.println("Thread died :/");
                     }
                     toHighlight.setBackground(original);
-                    toHighlight.paintImmediately(0, 0, getWidth(), getHeight());
+                    toHighlight.paintImmediately(0,0,getWidth(),getHeight());
+
                 }
             }
         }
-
-        Color tmp;
     }
-}
 
-class SimonButton extends JButton {
-    SimonButton(String text) {
+    SimonWindow(){
+        this.setSize(700, 700);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        this.setLayout(new BorderLayout());
+        replayButton = new JButton("Replay sequence");
+        startButton = new JButton("Play");
+        gridPanel = new JPanel();
+        buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout(1,2));
+        replayButton.setEnabled(false);
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                replayButton.setEnabled(true);
+                sequence = getSequence(DIFICULTY);
+                highlightSequence(500,new Color(0x03F825));
+//                gridPanel.paintImmediately(0,0,getWidth(),getHeight());
+                input = "";
+            }
+        });
+
+        gridPanel.setLayout(new GridLayout(3,3));
+        for (int i = 0; i < 9; i++) {
+            SimonButton toAdd = new SimonButton(String.valueOf(i+1));
+            gridPanel.add(toAdd);
+            buttons.add(toAdd);
+            toAdd.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    input += toAdd.getText();
+                    if (input.length() == DIFICULTY) {
+                        boolean win;
+                        if (sequence.equals(input)){
+                            win = true;
+                        } else {
+                            win = false;
+                        }
+                        evaluate(win);
+                        input = "";
+
+                    }
+                    System.out.println("Input: " + input);
+
+                }
+            });
+        }
+        this.add(buttonsPanel, BorderLayout.NORTH);
+        buttonsPanel.add(startButton);
+
+        buttonsPanel.add(replayButton);
+        replayButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                highlightSequence(1000,Color.orange);
+            }
+        });
+        this.add(gridPanel, BorderLayout.CENTER);
+    }
+
+    void evaluate(boolean win) {
+        int option = JOptionPane.showConfirmDialog(null, "tak si uplnej kokot", "test", JOptionPane.);
+        System.out.println(option);
+        if (option == 0){
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new SimonWindow().setVisible(true);
+    }
+
+    void resetGame() {
+        replayButton.setEnabled(true);
+        sequence = getSequence(DIFICULTY);
+        highlightSequence(500,new Color(0x03F825));
+    }
+
+} class SimonButton extends JButton{
+
+    SimonButton(String text){
         this.setText(text);
         this.setBackground(Color.cyan);
         this.setForeground(Color.black);
         this.setOpaque(true);
-        this.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        this.setBorder(BorderFactory.createLineBorder(Color.black,2));
         this.setFont(new Font("Consolas", Font.BOLD, 32));
         this.setFocusable(false);
     }
