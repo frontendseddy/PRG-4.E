be392032c81361beb937838da566f7cef29f694a
seddySedlak <pan.bramboru@seznam.cz>
2024-04-23 12:22:50 +0200

    File Recap

--- src/Practice/FileRecap.java
+++ src/Practice/FileRecap.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Recap {
+public class FileRecap {
 
     static void tree1(File file) {
         //projeti celeho adresare
